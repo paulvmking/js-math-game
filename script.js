9c62a0d9a9a615e8f7df008b0f5760c00a91adf4
paulvmking <paulvmking@gmail.com>
2021-02-15 17:29:16 +0000

    added addition function

--- script.js
+++ script.js
@@ -7,15 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked Submit")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame("addition");
 });
 
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting`;
+    }
+
+}
 }
 
 // Called when the user clicks the Submit button or presses Enter
@@ -38,7 +48,10 @@ function incrementWrongAnswer() {
 
 // Displays the questions.
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 
 }
 

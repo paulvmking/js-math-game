c4a6d2b2f2279f29eb744bed92f9393322dd0da3
paulvmking <paulvmking@gmail.com>
2021-02-15 17:14:12 +0000

    added event listeners

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-// The main game "loop", called when the script is first loaded
-// and after the user's answer has been processed
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit")
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 
 function runGame() {
 

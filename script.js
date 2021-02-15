c2444eb44696044e72d82e0b8f099fd45c993d86
paulvmking <paulvmking@gmail.com>
2021-02-15 17:05:59 +0000

    added empty functions

---
+++ script.js
@@ -0,0 +1,38 @@
+// The main game "loop", called when the script is first loaded
+// and after the user's answer has been processed
+
+function runGame() {
+
+}
+
+// Called when the user clicks the Submit button or presses Enter
+
+function checkAnswer() {
+
+}
+
+function calculateRightAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+// Displays the questions.
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}

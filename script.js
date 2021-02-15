7100372b5d45fb7590e0ecd571f44a6ce963b834
paulvmking <paulvmking@gmail.com>
2021-02-15 18:10:52 +0000

    added multiply functionality

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } 
+    else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type ${gameType}, aborting`;
@@ -55,21 +58,20 @@ function calculateRightAnswer() {
 
     if (operator === "+") { 
         return [operand1 + operand2, "addition"]; 
-    }
-    else {
+    } else if (operator === "x") { 
+        return [operand1 * operand2, "multiply"]; 
+    } else {
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator ${operator}, aborting`;
     }
 }
 
 function incrementScore() {
-
     let oldScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++oldScore;
 }
 
 function incrementWrongAnswer() {
-
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
 }
@@ -80,7 +82,6 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "+";
-
 }
 
 function displaySubtractQuestion() {
@@ -88,5 +89,7 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
